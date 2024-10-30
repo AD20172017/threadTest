7cf043255f6f0a3e92c38c5f747263a1702085db
byf <1765613885@qq.com>
2024-10-30 15:14:38 +0800

    共享指针使用伪闭包

--- session.cpp
+++ session.cpp
@@ -1,33 +1,39 @@
 #include "session.h"
-
-void session::handleRead(const boost::system::error_code ec, std::size_t bytesTransferred)
+#include "boost/uuid/uuid_generators.hpp"
+#include "boost/uuid/uuid_io.hpp"
+void session::handleRead(const boost::system::error_code ec, std::size_t bytesTransferred,session_ptr ptr)
 {
     if(!ec.value()){
         std::cout<<"Server receive data:"<<_data<<std::endl;
             async_write(_sock,buffer(_data,bytesTransferred),
-                std::bind(&session::handleWrite,this,std::placeholders::_1,std::placeholders::_2));
+                std::bind(&session::handleWrite,this,std::placeholders::_1,std::placeholders::_2,ptr));
             
     }else{
         std::cout<<"Read error\n";
-        delete this;
+        // delete this;
+        _ser->clearSession(_uuid);
+
     }
 }
 
-void session::handleWrite(const boost::system::error_code ec, std::size_t bytesTransferred)
+void session::handleWrite(const boost::system::error_code ec, std::size_t bytesTransferred,session_ptr ptr)
 {
     if(!ec.value()){
         memset(_data,0,max_length);
         _sock.async_read_some(buffer(_data,max_length),
-           std::bind(&session::handleRead,this,std::placeholders::_1,std::placeholders::_2));
+           std::bind(&session::handleRead,this,std::placeholders::_1,std::placeholders::_2,ptr));
 
     }else{
         std::cout<<"Write error\n";
-        delete this;
+        _ser->clearSession(_uuid);
     }
 }
 
-session::session(io_context &ioc) : _sock(ioc)
+session::session(io_context &ioc,server* ser) : _sock(ioc),_ser(ser)
 {
+    boost::uuids::uuid id=boost::uuids::random_generator()();
+    _uuid=boost::uuids::to_string(id);
+
 }
 
 session::~session()
@@ -39,30 +45,38 @@ void session::start()
 {
     memset(_data,0,max_length);
     _sock.async_read_some(buffer(_data,max_length),
-        std::bind(&session::handleRead,this,std::placeholders::_1,std::placeholders::_2));
+        std::bind(&session::handleRead,this,std::placeholders::_1,std::placeholders::_2,shared_from_this()));
 }
 
 void server::startAccept()
 {
-    auto newSesssion = new session(_ioc);
+    auto newSesssion = std::make_shared<session>(_ioc,this);
     _acceptor.async_accept(newSesssion->getSocket(),
         std::bind(&server::handleAccept,this,newSesssion,std::placeholders::_1));
 
 }
 
-void server::handleAccept(session* newSession, const boost::system::error_code &ec)
+void server::handleAccept(session_ptr newSession, const boost::system::error_code &ec)
 {
     if(!ec.value()){
         newSession->start();
+        _sessions.insert({newSession->getUuid(),newSession});
     }else{
-        delete newSession;
+        // delete newSession;
+        clearSession(newSession->getUuid());
+        std::cout << "session accept failed, error is " << ec.message() << std::endl;
     }
     startAccept();
 
 }
 
+void server::clearSession(std::string str)
+{   std::cout<<"clear uuid:"<<str<<std::endl;
+    _sessions.erase(str);
+}
+
 server::server(io_context &ioc, short port)
-    :_ioc(ioc),_acceptor(ioc,ip::tcp::endpoint(ip::tcp::v4(),port))
+    :_ioc(ioc),_acceptor(ioc,ip::tcp::endpoint(ip::tcp::v4(),port)),_port(port)
 {
     startAccept();
 }
--- session.h
+++ session.h
@@ -2,8 +2,16 @@
 #include <functional>
 #include "boost/asio.hpp"
 #include "boost/system/error_code.hpp"
+#include <memory>
+#include <map>
+
 using namespace boost::asio;
-class session
+
+class server;
+class session;
+using session_ptr=std::shared_ptr<session>;
+
+class session:public std::enable_shared_from_this<session>
 {
 private:
     ip::tcp::socket _sock;
@@ -11,31 +19,40 @@ private:
         max_length=1024
     };
     char _data[max_length];
-
-    void handleRead(const boost::system::error_code ec, std::size_t bytesTransferred);
-    void handleWrite(const boost::system::error_code ec, std::size_t bytesTransferred);
+    server* _ser;
+    std::string _uuid;
+    void handleRead(const boost::system::error_code ec, std::size_t bytesTransferred,
+        session_ptr ptr);
+    void handleWrite(const boost::system::error_code ec, std::size_t bytesTransferred,
+        session_ptr ptr);
 
 public:
-    session(io_context& ioc);
+    session(io_context& ioc,server* ser);
     ~session();
     ip::tcp::socket& getSocket(){
         return _sock;
     }
 
+    const std::string& getUuid(){
+        return _uuid;
+    }
     void start();
 };
 
 
 class server
-{
+{   
 private:
     void startAccept();
-    void handleAccept(session* newSession,const boost::system::error_code& ec);
+    void handleAccept(session_ptr newSession,const boost::system::error_code& ec);
+    void clearSession(std::string);
 
     io_context& _ioc;
     ip::tcp::acceptor _acceptor;
-
+    short _port;
+    std::map<std::string,std::shared_ptr<session>> _sessions;
 public:
     server(io_context& ioc,short port);
     ~server();
+    friend class session;
 };
